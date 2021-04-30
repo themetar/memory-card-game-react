bff3b5813fd4f5fe04b956dce931053f97d74563
Dimitar Apostolovski <d.apostolovski@gmail.com>
2021-04-30 21:25:13 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Memory Card
+
+[This project was bootstrapped with _Create React App_](./CRA-README.md)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Memory Card Game
+    </div>
+  );
+}
+
+export default App;
