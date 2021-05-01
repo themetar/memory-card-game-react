d8bac0f0d5346ee7b5d663cc5043fc4154d8854e
Dimitar Apostolovski <d.apostolovski@gmail.com>
2021-05-01 19:47:15 +0200

    Use React reducer to manage cards state

--- src/game/GameBoard.js
+++ src/game/GameBoard.js
@@ -1,5 +1,5 @@
 import './GameBoard.css';
-import {useState} from 'react';
+import {useState, useReducer} from 'react';
 import permutation from '../util/permutation';
 import Card from './Card';
 
@@ -11,6 +11,8 @@ const cardsBase = [
   {contents: "E", clicked: false},
 ];
 
+const START_COUNT = 5;
+
 function shuffleOrder(objects) {
   const getNextIndex = permutation(objects.length);
   objects.forEach(obj => {
@@ -19,8 +21,32 @@ function shuffleOrder(objects) {
   return objects;
 }
 
+function initCards(count) {
+  // deep copy 'count' number of items from cardsBase, then populate their order property
+  return shuffleOrder(cardsBase.slice(0, count).map(obj => ({...obj})));
+}
+
+function cardsStateReducer(cards, action) {
+  switch(true) {
+    case action === "reset":
+      /* Reset the board: return the cards to initial state */
+      return initCards(START_COUNT);
+
+    case typeof action === "number":
+      /* Player guessed a correct card */
+      const index = action;
+      const updatedCards = cards.slice();
+      updatedCards[index] = {...cards[index], clicked: true};
+      return shuffleOrder(updatedCards);
+
+    default:
+      /* coding error, unplanned case */
+      throw new Error(`action: ${action}`);
+  }
+}
+
 export default function GameBoard() {
-  const [cards, setCards] = useState(shuffleOrder(cardsBase));
+  const [cards, updateCards] = useReducer(cardsStateReducer, START_COUNT, initCards);
   
   const score = cards.reduce((acc, card) => acc + card.clicked, 0); // count clicked cards
 
@@ -32,12 +58,10 @@ export default function GameBoard() {
       // reset
       alert("Reset game");
       setBestScore(score > bestScore ? score : bestScore);
-      setCards(shuffleOrder(cards.map(card => ({...card, clicked: false}))));
+      updateCards("reset");
     } else {
       // score up
-      const updatedCards = cards.slice();
-      updatedCards[index] = {...cards[index], clicked: true};
-      setCards(shuffleOrder(updatedCards));
+      updateCards(index);
     }
   };
 
