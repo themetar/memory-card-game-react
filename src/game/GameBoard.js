91474bf8b107c33b6868b0e921164d152e8bab0c
Dimitar Apostolovski <d.apostolovski@gmail.com>
2021-05-04 19:20:11 +0200

    Add 'game over' state
    After player's wrong move, or when no moves remain (beaten game), set
    gameOver field to true. That triggers a message and a play again button to
    appear.
    Reducer now handles everything. Clik handler just calls state dispatch function.

--- src/game/GameBoard.js
+++ src/game/GameBoard.js
@@ -38,6 +38,7 @@ function initState(count) {
     cards: initCards(count),
     score: 0,
     bestScore: 0,
+    gameOver: false,
   }
 }
 
@@ -49,24 +50,38 @@ function stateReducer(state, action) {
         ...state,
         cards: initCards(START_COUNT),
         score: 0,
+        gameOver: false,
       };
 
     case typeof action === "number":
-      /* Player guessed a correct card */
+      /* Ignore if is game has ended */
+      if (state.gameOver) return state;
+      /* Player guessed a card */
       const index = action;
-      let updatedCards = state.cards.slice();
-      updatedCards[index] = {...state.cards[index], clicked: true};
-      // check for difficulty breakpoint
-      const score = state.score + 1;
-      const nextLevel = DIFFICULTY_LEVELS[score];
-      const attach = (nextLevel && cardsBase.slice(updatedCards.length, nextLevel).map(obj => ({...obj}))) || [];
-      updatedCards = updatedCards.concat(attach);
-      const bestScore = score > state.bestScore ? score : state.bestScore;
-      return {
-        score,
-        bestScore,
-        cards: shuffleOrder(updatedCards),
-      };
+      const card = state.cards[index];
+      if (card.clicked) {
+        /* Player guessed an incorrect card */
+        return {
+          ...state,
+          gameOver: true,
+        }
+      } else {
+        /* Player guessed a correct card */
+        let updatedCards = state.cards.slice();
+        updatedCards[index] = {...state.cards[index], clicked: true};
+        // check for difficulty breakpoint
+        const score = state.score + 1;
+        const nextLevel = DIFFICULTY_LEVELS[score];
+        const attach = (nextLevel && cardsBase.slice(updatedCards.length, nextLevel).map(obj => ({...obj}))) || [];
+        updatedCards = updatedCards.concat(attach);
+        const bestScore = score > state.bestScore ? score : state.bestScore;
+        return {
+          score,
+          bestScore,
+          cards: shuffleOrder(updatedCards),
+          gameOver: score === cardsBase.length, // all cards already clicked
+        };
+      }
 
     default:
       /* coding error, unplanned case */
@@ -76,18 +91,6 @@ function stateReducer(state, action) {
 
 export default function GameBoard() {
   const [state, dispatch] = useReducer(stateReducer, START_COUNT, initState);
-  
-  const clickCard = function clickCard(index) {
-    const {clicked} = state.cards[index];
-    if (clicked) {
-      // reset
-      alert("Reset game");
-      dispatch("reset");
-    } else {
-      // score up
-      dispatch(index);
-    }
-  };
 
   return (
     <div className="GameBoard">
@@ -95,9 +98,15 @@ export default function GameBoard() {
       <p>Score: {state.score}</p>
       <div className="cards-container">
         {state.cards.map((cardObj, i) =>
-          <Card key={i} {...cardObj} onClick={() => clickCard(i)} />
+          <Card key={i} {...cardObj} onClick={() => dispatch(i)} />
         )}
       </div>
+      {state.gameOver && (
+        <div>
+          <p>Game over</p>
+          <button onClick={() => dispatch("reset")}>Play Again</button>
+        </div>
+      )}
     </div>
   );
 }
