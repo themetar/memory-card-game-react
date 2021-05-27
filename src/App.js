75b2ad90dfe9588aee97d95cd704a75171d7539c
Dimitar Apostolovski <d.apostolovski@gmail.com>
2021-05-27 21:38:53 +0200

    Set up page header
    Add title and explainer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import GameBoard from './game/GameBoard';
 function App() {
   return (
     <div className="App">
-      <header>Memory Card Game</header>
+      <header>
+        <h1><span className="big">The Smurfs</span> <span className="small">Memory Game</span></h1>
+        <div className="brainy-explain">
+          <p>"Click <em>every</em> card, but only <em>once</em>."</p>
+          <img src="./smurfs/Brainy.webp" alt=""></img>
+        </div>
+      </header>
       <GameBoard />
     </div>
   );
