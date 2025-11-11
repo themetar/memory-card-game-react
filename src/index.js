29f6eb712a4ce4aef88e27c180f13530f1afe568
Dimitar Apostolovski <d.apostolovski@protonmail.com>
2025-11-11 15:01:33 +0100

    Move index.css importing to html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './index.css';
-import './fonts.css';
 import App from './App';
 
 const root = createRoot(document.getElementById("root"));
